376d7d0117b746b2d9de6ff0686c8e1f9072078f
Bonnie Appleton <bemappleton@gmail.com>
2020-09-18 10:49:10 +0100

    Number -> word for integers 0-9

--- src/number_word_converter/core.clj
+++ src/number_word_converter/core.clj
@@ -1,5 +1,17 @@
 (ns number-word-converter.core
   (:gen-class))
 
-(defn word-from-number [int]
-  "zero")
+(def integers-as-words
+  {:0 "zero"
+   :1 "one"
+   :2 "two"
+   :3 "three"
+   :4 "four"
+   :5 "five"
+   :6 "six"
+   :7 "seven"
+   :8 "eight"
+   :9 "nine"})
+
+(defn word-from-number [num]
+  ((keyword (str num)) integers-as-words))
--- test/number_word_converter/core_test.clj
+++ test/number_word_converter/core_test.clj
@@ -4,4 +4,8 @@
 
 (deftest number-word-conversion
   (testing "should return zero when 0 provided"
-    (is (=  (word-from-number 0) "zero"))))
+    (is (=  (word-from-number 0) "zero")))
+  (testing "should return one when 1 provided"
+    (is (=  (word-from-number 1) "one")))
+  (testing "should return nine when 9 provided"
+    (is (=  (word-from-number 9) "nine"))))
